599dc3e932e08d4650da5bb55f1954b2468ec9b5
Mahmood Badr <mahmoodbadr@outlook.com>
2022-07-21 19:04:53 -0400

    Functional

--- Iterative Binary Search Tool.py
+++ Iterative Binary Search Tool.py
@@ -1,12 +1,42 @@
-fulll_mailbox_list = [278, 512, 555, 987]
+arr = [278, 512, 555, 987]
 
-print("*" * 5 + " Mail Delivery Service " + "*" *5)
+print("*" * 5 + " Mail Delivery Service " + "*" * 5)
 print("Welcome to the Mail Delivery Service!")
-desired_flavour = input("What flavour would you lik? ")
+x = int(input("What mailbox number is yours? "))
 
-def flavour_search(icecream_flavours_list, desired_flavour):
-	for i in range(len(icecream_flavours_list)):
-		if icecream_flavours_list[i] == desired_flavour:
-			return True
-			print("We do have that flavour!")
-	return False
+# Python3 code to implement iterative Binary
+# Search.
+
+# It returns location of x in given array arr
+# if present, else returns -1
+
+
+def binarySearch(arr, l, r, x):
+
+	while l <= r:
+
+		mid = l + (r - l) // 2
+
+		# Check if x is present at mid
+		if arr[mid] == x:
+			return mid
+
+		# If x is greater, ignore left half
+		elif arr[mid] < x:
+			l = mid + 1
+
+		# If x is smaller, ignore right half
+		else:
+			r = mid - 1
+
+	# If we reach here, then the element
+	# was not present
+	return -1
+
+# Function call
+result = binarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1:
+	print("Element is present at index % d" % result)
+else:
+	print("Element is not present in array")
